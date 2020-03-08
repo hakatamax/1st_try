e7f162d8210ce30bbe780b9408eaff309575969c
takeshi murakami <hakatamax@gmail.com>
2020-03-08 11:12:13 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
+sprites.onDestroyed(SpriteKind.Food, function (sprite) {
+    mySprite2 = sprites.create(img`
+. . . . . . . . . . . 6 6 6 6 6 
+. . . . . . . . . 6 6 7 7 7 7 8 
+. . . . . . 8 8 8 7 7 8 8 6 8 8 
+. . e e e e c 6 6 8 8 . 8 7 8 . 
+. e 2 5 4 2 e c 8 . . . 6 7 8 . 
+e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
+e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
+e 2 e e 2 2 2 2 e e e e c 6 8 . 
+c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
+. c 2 e e e 2 e 2 4 2 2 2 2 c . 
+. . c 2 2 2 e e 2 2 2 2 2 2 2 e 
+. . . e c c e c 2 2 2 2 2 2 2 e 
+. . . . . . . c 2 e e 2 2 e 2 c 
+. . . . . . . c e e e e e e 2 c 
+. . . . . . . . c e 2 2 2 2 c . 
+. . . . . . . . . c c c c c . . 
+`, SpriteKind.Food)
+    mySprite2.setPosition(Math.randomRange(0, 80), Math.randomRange(0, 120))
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    mySprite2.destroy()
+    info.changeScoreBy(100)
+})
+let mySprite2: Sprite = null
 info.setScore(0)
 scene.setBackgroundColor(12)
 let mySprite = sprites.create(img`
@@ -19,7 +45,7 @@ c b d d c c b 5 5 5 5 5 5 5 b .
 . . . c c c c c c c c b b . . . 
 `, SpriteKind.Player)
 mySprite.setPosition(79, 111)
-let mySprite2 = sprites.create(img`
+mySprite2 = sprites.create(img`
 . . . . . . . . . . . 6 6 6 6 6 
 . . . . . . . . . 6 6 7 7 7 7 8 
 . . . . . . 8 8 8 7 7 8 8 6 8 8 
@@ -38,6 +64,7 @@ c 2 e e 2 2 2 2 e 2 5 4 2 c 8 .
 . . . . . . . . . c c c c c . . 
 `, SpriteKind.Food)
 mySprite2.setPosition(70, 5)
+game.splash("さくらんぼを食え！")
 forever(function () {
     controller.moveSprite(mySprite)
 })
